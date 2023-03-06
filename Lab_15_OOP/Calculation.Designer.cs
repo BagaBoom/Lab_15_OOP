6b01cce5286f42d6393968422c3bc0182697f708
Сергій Радчук <111866879+BagaBoom@users.noreply.github.com>
2023-03-06 23:44:28 +0200

    Добавьте файлы проекта.

---
+++ Lab_15_OOP/Calculation.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Lab_15_OOP
+{
+    partial class Calculation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.yieldLabel1 = new System.Windows.Forms.Label();
+            this.yield1 = new System.Windows.Forms.TextBox();
+            this.field1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.yieldLabel2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.yield2 = new System.Windows.Forms.TextBox();
+            this.field2 = new System.Windows.Forms.TextBox();
+            this.yieldLabel3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.yield3 = new System.Windows.Forms.TextBox();
+            this.field3 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.valueLabel = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.harvestSumLabel = new System.Windows.Forms.Label();
+            this.harvestSumTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Назад";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(226, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Обрахунок врожайності";
+            // 
+            // yieldLabel1
+            // 
+            this.yieldLabel1.AutoSize = true;
+            this.yieldLabel1.Location = new System.Drawing.Point(70, 164);
+            this.yieldLabel1.Name = "yieldLabel1";
+            this.yieldLabel1.Size = new System.Drawing.Size(188, 16);
+            this.yieldLabel1.TabIndex = 2;
+            this.yieldLabel1.Text = "Врожайність першого сорту";
+            // 
+            // yield1
+            // 
+            this.yield1.Location = new System.Drawing.Point(366, 161);
+            this.yield1.Name = "yield1";
+            this.yield1.Size = new System.Drawing.Size(100, 22);
+            this.yield1.TabIndex = 3;
+            // 
+            // field1
+            // 
+            this.field1.Location = new System.Drawing.Point(565, 161);
+            this.field1.Name = "field1";
+            this.field1.Size = new System.Drawing.Size(100, 22);
+            this.field1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(492, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Поле 1";
+            // 
+            // yieldLabel2
+            // 
+            this.yieldLabel2.AutoSize = true;
+            this.yieldLabel2.Location = new System.Drawing.Point(70, 205);
+            this.yieldLabel2.Name = "yieldLabel2";
+            this.yieldLabel2.Size = new System.Drawing.Size(185, 16);
+            this.yieldLabel2.TabIndex = 2;
+            this.yieldLabel2.Text = "Врожайність другого сорту";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(492, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Поле 2";
+            // 
+            // yield2
+            // 
+            this.yield2.Location = new System.Drawing.Point(366, 202);
+            this.yield2.Name = "yield2";
+            this.yield2.Size = new System.Drawing.Size(100, 22);
+            this.yield2.TabIndex = 3;
+            // 
+            // field2
+            // 
+            this.field2.Location = new System.Drawing.Point(565, 202);
+            this.field2.Name = "field2";
+            this.field2.Size = new System.Drawing.Size(100, 22);
+            this.field2.TabIndex = 4;
+            // 
+            // yieldLabel3
+            // 
+            this.yieldLabel3.AutoSize = true;
+            this.yieldLabel3.Location = new System.Drawing.Point(70, 249);
+            this.yieldLabel3.Name = "yieldLabel3";
+            this.yieldLabel3.Size = new System.Drawing.Size(233, 16);
+            this.yieldLabel3.TabIndex = 2;
+            this.yieldLabel3.Text = "Врожайність третього сорту сорту";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(492, 249);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 16);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Поле 3";
+            // 
+            // yield3
+            // 
+            this.yield3.Location = new System.Drawing.Point(366, 246);
+            this.yield3.Name = "yield3";
+            this.yield3.Size = new System.Drawing.Size(100, 22);
+            this.yield3.TabIndex = 3;
+            // 
+            // field3
+            // 
+            this.field3.Location = new System.Drawing.Point(565, 246);
+            this.field3.Name = "field3";
+            this.field3.Size = new System.Drawing.Size(100, 22);
+            this.field3.TabIndex = 4;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(588, 132);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(22, 16);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Га";
+            // 
+            // valueLabel
+            // 
+            this.valueLabel.AutoSize = true;
+            this.valueLabel.Location = new System.Drawing.Point(403, 132);
+            this.valueLabel.Name = "valueLabel";
+            this.valueLabel.Size = new System.Drawing.Size(40, 16);
+            this.valueLabel.TabIndex = 2;
+            this.valueLabel.Text = "ц / Га";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(303, 327);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 67);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Обрахувати";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(521, 327);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 67);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Обнулити";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // harvestSumLabel
+            // 
+            this.harvestSumLabel.AutoSize = true;
+            this.harvestSumLabel.Location = new System.Drawing.Point(70, 289);
+            this.harvestSumLabel.Name = "harvestSumLabel";
+            this.harvestSumLabel.Size = new System.Drawing.Size(0, 16);
+            this.harvestSumLabel.TabIndex = 2;
+            // 
+            // harvestSumTextBox
+            // 
+            this.harvestSumTextBox.Location = new System.Drawing.Point(366, 286);
+            this.harvestSumTextBox.Name = "harvestSumTextBox";
+            this.harvestSumTextBox.Size = new System.Drawing.Size(100, 22);
+            this.harvestSumTextBox.TabIndex = 3;
+            // 
+            // Calculation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 478);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.field3);
+            this.Controls.Add(this.field2);
+            this.Controls.Add(this.field1);
+            this.Controls.Add(this.harvestSumTextBox);
+            this.Controls.Add(this.yield3);
+            this.Controls.Add(this.yield2);
+            this.Controls.Add(this.yield1);
+            this.Controls.Add(this.valueLabel);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.harvestSumLabel);
+            this.Controls.Add(this.yieldLabel3);
+            this.Controls.Add(this.yieldLabel2);
+            this.Controls.Add(this.yieldLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Calculation";
+            this.Text = "Calculation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label yieldLabel1;
+        private System.Windows.Forms.TextBox yield1;
+        private System.Windows.Forms.TextBox field1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label yieldLabel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox yield2;
+        private System.Windows.Forms.TextBox field2;
+        private System.Windows.Forms.Label yieldLabel3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox yield3;
+        private System.Windows.Forms.TextBox field3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label valueLabel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label harvestSumLabel;
+        private System.Windows.Forms.TextBox harvestSumTextBox;
+    }
+}
---
+++ Lab_15_OOP/Calculation.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_15_OOP
+{
+    public partial class Calculation : Form
+    {
+        public Calculation()
+        {
+            InitializeComponent();
+            harvestSumTextBox.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            task1 tascOne = new task1();
+            tascOne.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            harvestSumTextBox.Hide();
+            harvestSumLabel.Text = "";
+            yieldLabel1.Text = "Врожайність першого сорту";
+            yieldLabel2.Text = "Врожайність другого сорту";
+            yieldLabel3.Text = "Врожайність третього сорту";
+            valueLabel.Text = "ц / Га";
+
+            yield1.Text = "";
+            yield2.Text = "";
+            yield3.Text = "";
+
+            field1.Text = "";
+            field2.Text = "";
+            field3.Text = "";
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double yieldOne = 0.0;
+            double yieldTwo = 0.0;
+            double yieldThree = 0.0;
+
+            double fieldOne = 0.0;
+            double fieldTwo = 0.0;
+            double fieldThree = 0.0;
+
+            try
+            {
+                yieldOne = double.Parse(yield1.Text);
+                yieldTwo = double.Parse(yield2.Text);
+                yieldThree = double.Parse(yield3.Text);
+
+                fieldOne = double.Parse(field1.Text);
+                fieldTwo = double.Parse(field2.Text);
+                fieldThree = double.Parse(field3.Text);
+            }
+            catch 
+            {
+                yield1.Text = "";
+                yield2.Text = "";
+                yield3.Text = "";
+
+                field1.Text = "";
+                field2.Text = "";
+                field3.Text = "";
+            }
+            yield1.Text = "";
+            yield2.Text = "";
+            yield3.Text = "";
+
+            field1.Text = "";
+            field2.Text = "";
+            field3.Text = "";
+
+            double harvestOne = 0.0;
+            double harvestTwo = 0.0;
+            double harvestThree = 0.0;
+            double harvestSum = 0.0;
+
+            if( yieldOne < 0 || yieldTwo < 0 || yieldThree < 0)
+            {
+                MessageBox.Show("Врожайність не може бути меншою 0.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if(fieldOne <= 0 || fieldTwo <= 0 || fieldThree <= 0)
+            {
+                MessageBox.Show("Площа поля не може бути від'ємною", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                harvestSumTextBox.Show();
+
+                yieldLabel1.Text = "Зібрано першого сорту";
+                yieldLabel2.Text = "Зібрано другого сорту";
+                yieldLabel3.Text = "Зібрано третього сорту";
+
+                valueLabel.Text = "Ц";
+
+                harvestSumLabel.Text = "Врожай разом";
+
+                harvestOne = yieldOne * fieldOne;
+                harvestTwo = yieldTwo * fieldTwo;
+                harvestThree = yieldThree * fieldThree;
+                harvestSum = harvestOne + harvestTwo + harvestThree;
+
+                yield1.Text = Math.Round(harvestOne, 2).ToString();
+                yield2.Text = Math.Round(harvestTwo, 2).ToString();
+                yield3.Text = Math.Round(harvestThree,2).ToString();
+
+                harvestSumTextBox.Text = Math.Round(harvestSum, 2).ToString();
+
+            }
+        }
+    }
+}
